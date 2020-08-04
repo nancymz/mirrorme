66cff86ec23b6728688dd578e211da087f2e61b8
nancymz <nancy.zacharias@outlook.com>
2020-08-03 22:42:49 -0700

    Updates

---
+++ precache-manifest.9019b42a48568a76bc8a77f9c73272f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b32ffce025b65bc6e643e4cf81f0c8f",
+    "url": "/mirrorme/index.html"
+  },
+  {
+    "revision": "2913bc88c3fd1e0bba1a",
+    "url": "/mirrorme/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "6c80a4915a8016c54788",
+    "url": "/mirrorme/static/js/2.e9965522.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mirrorme/static/js/2.e9965522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2913bc88c3fd1e0bba1a",
+    "url": "/mirrorme/static/js/main.abaf098b.chunk.js"
+  },
+  {
+    "revision": "9bcb7288c19fe5a5c427",
+    "url": "/mirrorme/static/js/runtime-main.78ee50dd.js"
+  }
+]);
